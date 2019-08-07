42d02dc5650689adf00c2a68819ee94c4958220d
Jared Ostyn <2815154+ostyn@users.noreply.github.com>
2019-08-07 01:03:39 -0600

    Adding a map to the view issue component

--- src/ViewIssue.js
+++ src/ViewIssue.js
@@ -4,6 +4,8 @@ import { DialogController, DialogService } from 'aurelia-dialog';
 import { IssueService } from './IssueService';
 import { EditIssue } from './EditIssue';
 import { Issue } from "./Issue";
+import * as L from 'leaflet';
+import "leaflet.icon.glyph/Leaflet.Icon.Glyph.js";
 @inject(DialogController, DialogService, IssueService)
 export class ViewIssue {
   constructor(controller, dialogService, issueService) {
@@ -14,6 +16,31 @@ export class ViewIssue {
   activate(issue) {
     if (issue)
       this.issue = issue;
+  }
+  attached() {
+    this.map = L.map('map', {zoomControl:false}).setView([this.issue.latlng.lat, this.issue.latlng.lng], 14);
+    let urlTemplate = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+    this.map.addLayer(L.tileLayer(urlTemplate, {
+      maxZoom: 19,
+      minZoom: 4
+    }));
+    new L.marker(this.issue.latlng, {icon: this.getIcon(this.issue) }).addTo(this.map);
+  }
+  getIcon(issue) {
+    let extraCss = this.selectedMarkerId == issue.id ? "" : "unselectedMarker"
+    L.Icon.Glyph.MDI = L.Icon.Glyph.extend({
+      options: {
+        prefix: 'fas',
+        iconUrl: 'marker.png',
+        shadowUrl: 'shadow.png',
+        iconAnchor: [24, 48],
+        shadowSize: [48, 48],
+        iconSize: [48, 48],
+        shadowAnchor: [0, 28]
+      }
+    });
+    L.icon.glyph.mdi = function (options) { return new L.Icon.Glyph.MDI(options); };
+    return L.icon.glyph.mdi({ className: extraCss, prefix: 'fas', glyph: Issue.mapping[issue.issueType].glyph, glyphSize: "18px", glyphAnchor: [0, -6] });
   }
   getIssueTypeGlyph(issueType) {
     return Issue.getIssueTypeGlyph(issueType);
